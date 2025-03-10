af9ba079390a5deb447e7ff95a3db94f14619ee6
Anna K <1415ks@ukr.net>
2025-03-11 00:26:47 +0200

    add right js

--- src/js/faq.js
+++ src/js/faq.js
@@ -1,2 +1,12 @@
 import Accordion from 'accordion-js';
 import 'accordion-js/dist/accordion.min.css';
+
+const accordionParams = {
+  openOnInit: [0],
+  duration: 400,
+  toggle: 'click',
+  easing: 'ease-in-out',
+  scrollTo: true,
+};
+
+new Accordion('.faq-container', accordionParams);
