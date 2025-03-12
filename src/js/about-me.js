8dd74f51d316a98227ad1d40737e16bafb537a73
maryna_bobryk <m.bobryk@web.de>
2025-03-12 15:14:44 +0100

    clean all partials

--- src/js/about-me.js
+++ src/js/about-me.js
@@ -1,6 +1,3 @@
-//import Swiper from 'swiper';
-// import Swiper from 'swiper/bundle';
-// import 'swiper/css/bundle';
 import Accordion from 'accordion-js';
 import 'accordion-js/dist/accordion.min.css';
 
@@ -15,27 +12,24 @@ const swiperSkills = new Swiper('.swiperr', {
   speed: 900,
   loop: true,
 
-   modules: [Navigation],
+  modules: [Navigation],
   navigation: {
     nextEl: '.skills-arrow-next',
   },
-
-  // Default parameters
   slidesPerView: 2,
   spaceBetween: 0,
-  // Responsive breakpoints
+
   breakpoints: {
-    // when window width is >= 320px
     320: {
       slidesPerView: 2,
       spaceBetween: 0,
     },
-    // when window width is >= 480px
+
     768: {
       slidesPerView: 3,
       spaceBetween: 0,
     },
-    // when window width is >= 640px
+
     1440: {
       slidesPerView: 6,
       spaceBetween: 0,
@@ -54,7 +48,7 @@ const accordionParams = {
   duration: 400,
   toggle: 'click',
   easing: 'ease-in-out',
-  scrollTo: true, // Прокручує до відкритого елемента
+  scrollTo: true,
 };
 
 new Accordion('.content-container', accordionParams);
