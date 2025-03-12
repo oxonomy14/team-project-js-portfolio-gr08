98f7df8268e6b3a483140c1d6180650a9fc5f96d
Andrii Semenenko <oxonomy14@gmail.com>
2025-03-13 00:39:01 +0700

    fix css & js

--- src/js/projects.js
+++ src/js/projects.js
@@ -3,9 +3,6 @@ import 'swiper/css/navigation';
 import Swiper from 'swiper';
 import { Navigation } from 'swiper/modules';
 
-const elemProjectBtnNext = document.querySelector('.swiper-project-btn-next');
-const elemProjectBtnPrev = document.querySelector('.swiper-project-btn-prev');
-
 const swiperProject = new Swiper('.swiperrr', {
   modules: [Navigation],
   slidesPerView: 1,
@@ -13,16 +10,16 @@ const swiperProject = new Swiper('.swiperrr', {
   speed: 900,
   spaceBetween: 400,
 
-  touchRatio: 1, 
-  allowTouchMove: true, 
+  touchRatio: 1,
+  allowTouchMove: true,
 
   navigation: {
-    nextEl: '.reviews-swiper-button-next',
-    prevEl: '.reviews-swiper-button-prev',
+    nextEl: '.swiper-project-btn-next',
+    prevEl: '.swiper-project-btn-prev',
   },
 });
 
-  document.addEventListener('keydown', evt => {
+document.addEventListener('keydown', evt => {
   if (evt.code === 'ArrowRight') {
     swiperProject.slideNext();
   } else if (evt.code === 'ArrowLeft') {
