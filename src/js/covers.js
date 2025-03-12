70ad2483f2c2ec11e4e9b99b4b49f857e34f5039
Maksym-Shchepetkov <mama2009113@gmail.com>
2025-03-12 18:48:32 +0200

    add images update html css js markup

--- src/js/covers.js
+++ src/js/covers.js
@@ -1,20 +1,22 @@
 document.addEventListener('DOMContentLoaded', function () {
   const coverSection = document.querySelector('.cover-wrapper');
   const coverImages = document.querySelectorAll('.cover-img');
+  const coverList = document.querySelector('.cover-list');
+  const coverItems = document.querySelector('.cover-items');
 
   if (!coverSection) return;
 
   const observer = new IntersectionObserver(
     entries => {
       entries.forEach(entry => {
-        if (entry.isIntersecting > 0.4) {
+        if (entry.isIntersecting > 0.1) {
           coverImages.forEach(image => image.classList.add('in-view'));
         } else {
           coverImages.forEach(image => image.classList.remove('in-view'));
         }
       });
     },
-    { threshold: 0.4 }
+    { threshold: 0.1 }
   );
 
   observer.observe(coverSection);
